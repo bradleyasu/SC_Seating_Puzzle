9aa098e44573ae49665065ee000088443464dce3
Brad <bradleyasu@gmail.com>
2019-05-07 17:28:36 -0400

    Additional Comments

--- test/com/showclix/testsuite/TestSuite.java
+++ test/com/showclix/testsuite/TestSuite.java
@@ -1,11 +1,21 @@
 package com.showclix.testsuite;
 
+/**
+ * The TestSuite is a wrapper class that will execute all of the other jUnit
+ * tests
+ * 
+ * @author Bradley Sheets
+ * @version 1.0
+ * @date May 7, 2019
+ * 
+ */
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import com.showclix.seating.SeatTest;
 import com.showclix.seating.SeatingTest;
 
+// List of test classes to execute
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	SeatTest.class,
